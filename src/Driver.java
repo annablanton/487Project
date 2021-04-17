86e7a809c4343580c388062b9f1c3b34700ebdce
Anna Blanton <blanton@uw.edu>
2021-04-16 19:58:58 -0700

    Added C code to work from, began converting to Java

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,8 @@
 public class Driver {
     public static void main(String[] args) {
-        System.out.println(Utilities.right_encode(256));
-        System.out.println(Utilities.left_encode(256));
-        System.out.println(Utilities.encode_string(""));
-        System.out.println(Utilities.bytepad(Utilities.encode_string(""), 20));
+        System.out.println(SHA3.right_encode(256));
+        System.out.println(SHA3.left_encode(256));
+        System.out.println(SHA3.encode_string(""));
+        System.out.println(SHA3.bytepad(SHA3.encode_string(""), 20));
     }
 }
