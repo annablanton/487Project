cfa802512575572839715a7e6de3a46931b51197
Anna Blanton <blanton@uw.edu>
2021-04-24 22:18:46 -0700

    Revert "Updated encoding functions to return byte arrays, began converting C main to Java"
    This reverts commit 7dc2b0096577d107fdc83229c45eb6a4ab063224.

--- src/SHA3.java
+++ src/SHA3.java
@@ -1,73 +1,66 @@
 public class SHA3 {
     //SHA-3 implementation converted from C to Java from https://github.com/mjosaarinen/tiny_sha3
-    public static final int KECCAKF_ROUNDS = 24;
+    public static final int KECCAKF_ROUNDS 24
 
     // state context
-    byte[] b = new byte[200];
-    int pt, rsiz, mdlen;
-    boolean ext = true;
+    public static byte[] b = new byte[200];
+    static int pt, rsiz, mdlen;
 
     public static long ROTL64(long x, long y) {
         return (((x) << (y)) | ((x) >>> (64 - (y))));
     }
 
-    private static final long[/*24*/] keccakf_rndc = {
+    static void sha3_keccakf(long st[25])
+    {
+        // constants
+        long[/*24*/] keccakf_rndc = {
             0x0000000000000001L, 0x0000000000008082L, 0x800000000000808aL,
-            0x8000000080008000L, 0x000000000000808bL, 0x0000000080000001L,
-            0x8000000080008081L, 0x8000000000008009L, 0x000000000000008aL,
-            0x0000000000000088L, 0x0000000080008009L, 0x000000008000000aL,
-            0x000000008000808bL, 0x800000000000008bL, 0x8000000000008089L,
-            0x8000000000008003L, 0x8000000000008002L, 0x8000000000000080L,
-            0x000000000000800aL, 0x800000008000000aL, 0x8000000080008081L,
-            0x8000000000008080L, 0x0000000080000001L, 0x8000000080008008L
-    };
-    private static final int[/*24*/] keccakf_rotc = {
+                    0x8000000080008000L, 0x000000000000808bL, 0x0000000080000001L,
+                    0x8000000080008081L, 0x8000000000008009L, 0x000000000000008aL,
+                    0x0000000000000088L, 0x0000000080008009L, 0x000000008000000aL,
+                    0x000000008000808bL, 0x800000000000008bL, 0x8000000000008089L,
+                    0x8000000000008003L, 0x8000000000008002L, 0x8000000000000080L,
+                    0x000000000000800aL, 0x800000008000000aL, 0x8000000080008081L,
+                    0x8000000000008080L, 0x0000000080000001L, 0x8000000080008008L
+        };
+        int[/*24*/] keccakf_rotc = {
             1,  3,  6,  10, 15, 21, 28, 36, 45, 55, 2,  14,
-            27, 41, 56, 8,  25, 43, 62, 18, 39, 61, 20, 44
-    };
-    private static final int[/*24*/] keccakf_piln = {
+                    27, 41, 56, 8,  25, 43, 62, 18, 39, 61, 20, 44
+        };
+        int[/*24*/] keccakf_piln = {
             10, 7,  11, 17, 18, 3, 5,  16, 8,  21, 24, 4,
-            15, 23, 19, 13, 12, 2, 20, 14, 22, 9,  6,  1
-    };
-
-    public static long ROTL64(long x, int y) {
-        return (x << y) | (x >>> (64 - (y)));
-    }
-
-    private static void sha3_keccakf(byte[/*200*/] v){
-        long[] st = new long[25];
-        long[] bc = new long[5];
+                    15, 23, 19, 13, 12, 2, 20, 14, 22, 9,  6,  1
+        };
 
         // variables
         int i, j, r;
         long t;
+        long[/*5*/] bc;
 
-        // #if __BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__
-        //     uint8_t *v;
-        // endianess conversion. this is redundant on little-endian targets
-        for (i = 0, j =0; i < 25; i++, j +=8) {
-            // v = (uint8_t *) &st[i];
-            st[i] =  (((long)v[j + 0] & 0xFFL)) | (((long)v[j + 1] & 0xFFL) <<  8) |
-                    (((long)v[j + 2] & 0xFFL) << 16) | (((long)v[j + 3] & 0xFFL) << 24) |
-                    (((long)v[j + 4] & 0xFFL) << 32) | (((long)v[j + 5] & 0xFFL) << 40) |
-                    (((long)v[j + 6] & 0xFFL) << 48) | (((long)v[j + 7] & 0xFFL) << 56);
-        }
-        // #endif
+        #if __BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__
+            uint8_t *v;
+
+            // endianess conversion. this is redundant on little-endian targets
+            for (i = 0; i < 25; i++) {
+                v = (uint8_t *) &st[i];
+                st[i] = ((uint64_t) v[0])     | (((uint64_t) v[1]) << 8) |
+                        (((uint64_t) v[2]) << 16) | (((uint64_t) v[3]) << 24) |
+                        (((uint64_t) v[4]) << 32) | (((uint64_t) v[5]) << 40) |
+                        (((uint64_t) v[6]) << 48) | (((uint64_t) v[7]) << 56);
+            }
+        #endif
 
         // actual iteration
         for (r = 0; r < KECCAKF_ROUNDS; r++) {
 
             // Theta
-            for (i = 0; i < 5; i++){
+            for (i = 0; i < 5; i++)
                 bc[i] = st[i] ^ st[i + 5] ^ st[i + 10] ^ st[i + 15] ^ st[i + 20];
-
-            }
 
             for (i = 0; i < 5; i++) {
                 t = bc[(i + 4) % 5] ^ ROTL64(bc[(i + 1) % 5], 1);
-                for (j = 0; j < 25; j += 5){
+                for (j = 0; j < 25; j += 5)
                     st[j + i] ^= t;
-                }
             }
 
             // Rho Pi
@@ -81,38 +74,36 @@ public class SHA3 {
 
             //  Chi
             for (j = 0; j < 25; j += 5) {
-                for (i = 0; i < 5; i++){
+                for (i = 0; i < 5; i++)
                     bc[i] = st[j + i];
-                }
-                for (i = 0; i < 5; i++){
+                for (i = 0; i < 5; i++)
                     st[j + i] ^= (~bc[(i + 1) % 5]) & bc[(i + 2) % 5];
-                }
             }
 
             //  Iota
             st[0] ^= keccakf_rndc[r];
         }
 
-        // #if __BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__
-        // endianess conversion. this is redundant on little-endian targets
-        for (i = 0, j = 0 ; i < 25; i++, j+=8) {
-            // v = (uint8_t *) &st[i];
-            t = st[i];
-            v[0] = (byte)(t & 0xFF);
-            v[1] = (byte)((t >>> 8) & 0xFF);
-            v[2] = (byte)((t >>> 16) & 0xFF);
-            v[3] = (byte)((t >>> 24) & 0xFF);
-            v[4] = (byte)((t >>> 32) & 0xFF);
-            v[5] = (byte)((t >>> 40) & 0xFF);
-            v[6] = (byte)((t >>> 48) & 0xFF);
-            v[7] = (byte)((t >>> 56) & 0xFF);
-        }
-        // #endif
+        #if __BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__
+            // endianess conversion. this is redundant on little-endian targets
+            for (i = 0; i < 25; i++) {
+                v = (uint8_t *) &st[i];
+                t = st[i];
+                v[0] = t & 0xFF;
+                v[1] = (t >>> 8) & 0xFF;
+                v[2] = (t >>> 16) & 0xFF;
+                v[3] = (t >>> 24) & 0xFF;
+                v[4] = (t >>> 32) & 0xFF;
+                v[5] = (t >>> 40) & 0xFF;
+                v[6] = (t >>> 48) & 0xFF;
+                v[7] = (t >>> 56) & 0xFF;
+            }
+        #endif
     }
 
     // Initialize the context for SHA3
 
-    void sha3_init(int mdlen)
+    static void sha3_init(int mdlen)
     {
         int i;
 
@@ -125,7 +116,7 @@ public class SHA3 {
 
     // update state with more data
 
-    void sha3_update(byte[] data, int len)
+    static void sha3_update(byte[] data, int len)
     {
         int i;
         int j;
@@ -134,7 +125,7 @@ public class SHA3 {
         for (i = 0; i < len; i++) {
             b[j++] ^= data[i];
             if (j >= rsiz) {
-                sha3_keccakf(b);
+                sha3_keccakf(c->st.q);
                 j = 0;
             }
         }
@@ -143,42 +134,44 @@ public class SHA3 {
 
     // finalize and output a hash
 
-    void sha3_final(byte[] md)
+    static byte[] sha3_final(byte[] md)
     {
         int i;
 
         b[pt] ^= 0x06;
         b[rsiz - 1] ^= 0x80;
-        sha3_keccakf(b);
+        sha3_keccakf(c->st.q);
 
         for (i = 0; i < mdlen; i++) {
             md[i] = b[i];
         }
+
+        return md;
     }
 
     // compute a SHA-3 hash (md) of given byte length from "in"
 
-    byte[] sha3(byte[] in, int inlen, byte[] md, int mdlen)
+    static byte[] sha3(byte[] in, int inlen, byte[] md, int mdlen)
     {
 
         sha3_init(mdlen);
         sha3_update(in, inlen);
-        sha3_final(md);
+        md = sha3_final(md);
 
         return md;
     }
 
     // SHAKE128 and SHAKE256 extensible-output functionality
 
-    void shake_xof()
+    static void shake_xof()
     {
-        b[pt] ^= (byte) (this.ext ? 0x04 : 0x1F);
+        b[pt] ^= 0x1F;
         b[rsiz - 1] ^= 0x80;
-        sha3_keccakf(b);
+        sha3_keccakf(c->st.q);
         pt = 0;
     }
 
-    void shake_out(byte[] out, int len)
+    static void shake_out(byte[] out, int len)
     {
         int i;
         int j;
@@ -186,7 +179,7 @@ public class SHA3 {
         j = pt;
         for (i = 0; i < len; i++) {
             if (j >= rsiz) {
-                sha3_keccakf(b);
+                sha3_keccakf(c->st.q);
                 j = 0;
             }
             out[i] = b[j++];
@@ -196,7 +189,7 @@ public class SHA3 {
 
 
 
-    public static byte[] right_encode(long x) {
+    public static String right_encode(long x) {
         byte n;
         if (x == 0) {
             n = 1;
@@ -209,33 +202,31 @@ public class SHA3 {
             }
         }
 
-        byte[] xArr = new byte[n+1];
+        byte[] xArr = new byte[n];
 
         for (int i = 0; i < n; i++) {
             long mask = 0xFF << (8 * (n - i - 1));
             byte x_i = (byte) ((x & mask) >>> (8 * (n - i - 1)));
             xArr[i] = x_i;
         }
-        xArr[n] = n;
-        return xArr;
 
-//        String[] oArr = new String[n+1];
+        String[] oArr = new String[n+1];
 
-//        for (int i = 0; i < n; i++) {
-//            oArr[i] = enc8(xArr[i]);
-//        }
-//
-//        oArr[n] = enc8(n);
-//        String O = "";
-//        for (int i = 0; i < n+1; i++) {
-//            O = O.concat(oArr[i]);
-//        }
-//
-//        return O;
+        for (int i = 0; i < n; i++) {
+            oArr[i] = enc8(xArr[i]);
+        }
+
+        oArr[n] = enc8(n);
+        String O = "";
+        for (int i = 0; i < n+1; i++) {
+            O = O.concat(oArr[i]);
+        }
+
+        return O;
 
     }
 
-    public static byte[] left_encode(long x) {
+    public static String left_encode(long x) {
         byte n;
         if (x == 0) {
             n = 1;
@@ -248,68 +239,57 @@ public class SHA3 {
             }
         }
 
-        byte[] xArr = new byte[n+1];
-        xArr[0] = n;
+        byte[] xArr = new byte[n];
 
         for (int i = 0; i < n; i++) {
             long mask = 0xFF << (8 * (n - i - 1));
             byte x_i = (byte) ((x & mask) >>> (8 * (n - i - 1)));
-            xArr[i+1] = x_i;
+            xArr[i] = x_i;
         }
 
-        return xArr;
+        String[] oArr = new String[n+1];
 
-//        String[] oArr = new String[n+1];
-//
-//        oArr[0] = enc8(n);
-//        for (int i = 0; i < n; i++) {
-//            oArr[i+1] = enc8(xArr[i]);
-//        }
-//
-//        String O = "";
-//        for (int i = 0; i < n+1; i++) {
-//            O = O.concat(oArr[i]);
-//        }
-//
-//        return O;
+        oArr[0] = enc8(n);
+        for (int i = 0; i < n; i++) {
+            oArr[i+1] = enc8(xArr[i]);
+        }
+
+        String O = "";
+        for (int i = 0; i < n+1; i++) {
+            O = O.concat(oArr[i]);
+        }
+
+        return O;
     }
 
-    public static byte[] encode_string(byte[] S) {
-        long slen = S.length;
-        long x = 0;
-        for (int i = S.length - 1; i >= 0; i--) {
-            x += S[i] << (8 * i);
-        }
-
-        int i = 0;
-        x += slen << (8 * S.length);
-        return left_encode(x);
+    public static String encode_string(String S) {
+        return left_encode(S.length()).concat(S);
     }
 
-    public static byte[] bytepad(byte[] X, int w) {
-        byte[] wenc = left_encode(w);
-        byte[] z = new byte[wenc.length + X.length + w - ((wenc.length + X.length) % w)];
-        for (int i = 0; i < wenc.length; i++) {
-            z[i] = wenc[i];
+    public static String bytepad(String X, long w) {
+        String z = left_encode(w).concat(X);
+        while (z.length() % 8 != 0) {
+            z = z.concat("0");
         }
-        for (int i = wenc.length, j = 0; i < wenc.length + X.length; i++, j++) {
-            z[i] = X[j];
+
+        while ((z.length() / 8) % w != 0) {
+            z = z.concat("00000000");
         }
 
         return z;
     }
 
-//    private static byte enc8(byte b) {
-//        StringBuilder ret = new StringBuilder();
-//        for (int i = 0; i < 8; i++) {
-//            if (b != 0) {
-//                ret.append(Integer.toString(Math.abs(b % 2)));
-//                b >>>= 1;
-//            } else {
-//                ret.append("0");
-//            }
-//        }
-//
-//        return ret.toString();
-//    }
+    private static String enc8(byte b) {
+        StringBuilder ret = new StringBuilder();
+        for (int i = 0; i < 8; i++) {
+            if (b != 0) {
+                ret.append(Integer.toString(Math.abs(b % 2)));
+                b >>>= 1;
+            } else {
+                ret.append("0");
+            }
+        }
+
+        return ret.toString();
+    }
 }
